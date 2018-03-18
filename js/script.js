690a06504690b6791d6e7a0c4062c0932b0232dc
Vasil Kotsev <vasil.kotsev@gmail.com>
2018-03-19 01:13:43 +0200

    added jquery settings

--- js/script.js
+++ js/script.js
@@ -1,6 +1,25 @@
-/* Carousel custom settings */
 
+/* Carousel settings */
 $('.carousel').carousel({
     interval: 3000,
     pause: false
-})
+});
+
+
+/* Page JQ settings */
+$(document).ready(function () {
+
+    function navMenuToggle (clicked, hide, show) {
+        $(clicked).click(function () {
+            $(hide).hide();
+            $(show).show();
+        });
+    }
+
+    navMenuToggle(".nav-item:nth-child(1)", '.second-article, .first-action-dropdown, .second-action-dropdown, .third-action-dropdown', '.index');
+    navMenuToggle(".nav-item:nth-child(2)", '.index, .first-action-dropdown, .second-action-dropdown, .third-action-dropdown', '.second-article');
+    navMenuToggle(".dropdown-item:nth-of-type(1)", '.index, .second-article, .second-action-dropdown, .third-action-dropdown', '.first-action-dropdown');
+    navMenuToggle(".dropdown-item:nth-of-type(2)", '.index, .second-article, .first-action-dropdown, .third-action-dropdown', '.second-action-dropdown');
+    navMenuToggle(".dropdown-item:nth-of-type(3)", '.index, .second-article, .first-action-dropdown, .second-action-dropdown', '.third-action-dropdown');
+
+});
